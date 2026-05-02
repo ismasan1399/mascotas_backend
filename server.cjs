7882544d23fb11cf62c3ac7dd07bc48532ccecaf
Jorge Fausto Hernandez <jorgehdezmxgdl@gmail.com>
2026-05-02 14:31:08 -0600

    Backend

--- server.cjs
+++ server.cjs
@@ -1,9 +1,13 @@
-const express = require('express')
+const express   = require('express');
 const { supabase, testConnection, insertaMascota } = require('./db/conexion.cjs');
-
+const bcrypt    = require('bcrypt');
+const jwt      = require('jsonwebtoken');
+const cors = require('cors');
+const SALT_ROUNDS_BCRYPT = 15;
 const app = express()
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -33,6 +37,73 @@ app.get('/mascotas/:id', async (req, res) => {
         res.status(200).json({ok: true, message: "Mascota no encontrada"});  
      }
    }  
+});
+
+
+app.get('/crea_cuenta', async (req, res) => {
+    const dueno_id = 3;
+    const email    = "micorreo3@correo.com";
+    const pass     = "asdfghjk";
+    const pass_hash= await bcrypt.hash(pass, SALT_ROUNDS_BCRYPT);
+    console.log("Hash generado: ", pass_hash);
+    const {data, error} = await supabase
+         .from('usuarios')
+         .insert([
+            {
+                dueno_id,
+                email,
+                password_hash: pass_hash,
+                activo: true,
+                ultimo_login: new Date().toISOString(),
+                created_at: new Date().toISOString(),
+            }
+         ])
+         .select('id, dueno_id, email, activo, ultimo_login')
+         .single();
+    if (error) {
+        res.status(503).json({ok: false, message: "Error al conectar con la base de datos: " + error.message});
+    } else {
+        res.status(200).json({ok: true, message: "Cuenta creada exitosamente", data});
+    }
+});
+
+app.post('/auth/login', async(req, res)  => {
+   try 
+   {
+     const {email, password} = req.body;
+     console.log(req.body);
+     if (!email || !password) {
+        res.status(200).json({ok: false, message: "Faltan datos de ingreso"});
+        return;
+     }
+    const {data: usuario, error} = await supabase
+        .from('usuarios')    
+        .select('*')
+        .eq('email', email)
+        .maybeSingle();
+    if (error) {
+        res.status(503).json({ok: false, message: "Error al conectar con la base de datos: " + error.message});
+        return;
+    }
+    if (!usuario) {
+        res.status(404).json({ok: false, message: "Usuario no encontrado"});
+        return;
+    }
+    console.log("Usuario encontrado: ", usuario);
+    const match = await bcrypt.compare(password, usuario.password_hash);
+    if (!match) {
+        res.status(401).json({ok: false, message: "Credenciales incorrectas"});
+        return;
+    }
+    const token = jwt.sign({
+        dueno_id: usuario.dueno_id,
+    }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: "1h" });
+    res.status(200).json({ok: true, message: "Login exitoso", token});
+   } catch (e) {
+      res.status(503).json({ok: false, message: "Error al conectar con la base de datos: " + e.message});
+   }
 });
 
 
